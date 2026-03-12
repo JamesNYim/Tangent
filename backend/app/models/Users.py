c415320cfb3139612725bdd25a3b54f818cd8f4f
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2026-03-12 13:35:38 -0700

    Created Users db model

---
+++ backend/app/models/Users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
+from ../db/db import Base
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    api_key = Column(String, nullable=True);
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    conversations = relationship("Conversation", back_populates="user", cascade="all, delete-orphan")
